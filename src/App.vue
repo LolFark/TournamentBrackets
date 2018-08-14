07b2ade773410cca1449169c5e4e7667295f0c22
Eric Zhao <ericzhao94@gmail.com>
2018-08-14 17:43:32 +1000

    add player functionality

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1 class="Title">Player entry</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Players></Players>
+  </div>
+</template>
+
+<script>
+import Players from './components/Players.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Players
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  .Title {
+    font-size: 50px;
+  }
+</style>
---
+++ src/components/Players.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="players">
+    <h1>{{ msg }}</h1>
+    <div class="holder">
+      <form @submit.prevent="addPlayer">
+        <input type="text" placeholder="Enter player name.." v-model="player">
+        <p>{{ player }}</p>
+      </form>
+      <ul>
+        <li v-for="player in currentPlayers">{{ player.text }}</li>
+      </ul>
+      <p v-if="currentPlayers.length < 1">No players added</p>
+      <p v-else># of players: {{ currentPlayers.length}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'players',
+        data() {
+            return {
+                player: "",
+                msg: 'Add players',
+                currentPlayers: [
+                ]
+            }
+        },
+        methods: {
+            addPlayer() {
+                this.currentPlayers.push({text: this.player});
+                this.player = "";
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    font-weight: bold;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
